9e70c0cd3f37f0d5bbcb9a75bb0f18fad87d760c
Marcus Mimnagh <marcusmimnagh@Marcuss-MacBook-Pro.local>
2020-04-29 12:35:43 -0500

    Updates

---
+++ precache-manifest.a824f552184a866f3d021395b0fec48c.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "451a5cabd2f7d3e97ca86892df8413f5",
+    "url": "/roboreact/index.html"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/roboreact/static/css/2.036b41f9.chunk.css"
+  },
+  {
+    "revision": "380ec651d0a34b9aa749",
+    "url": "/roboreact/static/css/main.bcc819b5.chunk.css"
+  },
+  {
+    "revision": "d1d700ca0dae124b0e5e",
+    "url": "/roboreact/static/js/2.2b73e027.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/roboreact/static/js/2.2b73e027.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "380ec651d0a34b9aa749",
+    "url": "/roboreact/static/js/main.a8e7eabd.chunk.js"
+  },
+  {
+    "revision": "bd170a4a4c5e42933a74",
+    "url": "/roboreact/static/js/runtime-main.de0a0211.js"
+  },
+  {
+    "revision": "296ffb102adbb763d6e455cc28391674",
+    "url": "/roboreact/static/media/Starcraft Normal.296ffb10.woff"
+  }
+]);
